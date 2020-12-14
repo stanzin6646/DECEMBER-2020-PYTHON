b825b3aaec749046744000632a9e1f65ef0b9358
Stanzin Nurboo <71585087+stanzin6646@users.noreply.github.com>
2020-12-14 12:29:26 +0530

    program to print all positive no

---
+++ printing_+no.py
@@ -0,0 +1,17 @@
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14,-95, 3]
+
+
+print("list1 = ", list1)
+for num in list1: 
+      
+   if num >= 0:
+      
+       print(num, end = " ")
+
+       
+print("list2 = ", list2)     
+for num in list2: 
+      
+   if num >= 0: 
+       print(num, end = " ") 
